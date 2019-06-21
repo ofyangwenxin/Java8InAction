d2a36f68700696d7faf24eab59bdb7c0e5f62fc9
yangwenxin <yangwenxin45@gmail.com>
2019-06-21 09:16:49 +0800

    行为参数化实践

--- src/main/java/chap2/FilteringApples.java
+++ src/main/java/chap2/FilteringApples.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FilteringApples {
@@ -20,7 +21,7 @@ public class FilteringApples {
     }
 
     /**
-     * 筛选绿苹果
+     * 第一次尝试
      * @param inventory
      * @return
      */
@@ -34,5 +35,146 @@ public class FilteringApples {
         return result;
     }
 
-//    public static List<Apple> filterApplesByColor(List)
+    /**
+     * 第二次尝试
+     *
+     * @param inventory
+     * @param color
+     * @return
+     */
+    public static List<Apple> filterApplesByColor(List<Apple> inventory,
+                                                  String color) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor().equals(color)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 第二次尝试
+     *
+     * @param inventory
+     * @param weight
+     * @return
+     */
+    public static List<Apple> filterApplesByWeight(List<Apple> inventory,
+                                                   int weight) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getWeight() > weight) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 第三次尝试
+     *
+     * @param inventory
+     * @param color
+     * @param weight
+     * @param flag
+     * @return
+     */
+    public static List<Apple> filterApples(List<Apple> inventory, String color,
+                                           int weight, boolean flag) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if ((flag && apple.getColor().equals(color)) ||
+                    (!flag && apple.getWeight() > weight)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public interface ApplePredicate {
+        boolean test(Apple apple);
+    }
+
+    public static class AppleHeavyWeightPredicate implements ApplePredicate {
+        @Override
+        public boolean test(Apple apple) {
+            return apple.getWeight() > 150;
+        }
+    }
+
+    public static class AppleGreenColorPredicate implements ApplePredicate {
+        @Override
+        public boolean test(Apple apple) {
+            return "green".equals(apple.getColor());
+        }
+    }
+
+    /**
+     * 第四次尝试
+     *
+     * @param inventory
+     * @param p
+     * @return
+     */
+    public static List<Apple> filterApples(List<Apple> inventory,
+                                           ApplePredicate p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String... args) {
+
+        List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red"));
+
+        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        List<Apple> greenApples = filterApplesByColor(inventory, "green");
+        System.out.println(greenApples);
+
+        // [Apple{color='red', weight=120}]
+        List<Apple> redApples = filterApplesByColor(inventory, "red");
+        System.out.println(redApples);
+
+        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        List<Apple> greenApples2 = filterApples(inventory, new AppleGreenColorPredicate());
+        System.out.println(greenApples2);
+
+        // [Apple{color='green', weight=155}]
+        List<Apple> heavyApples = filterApples(inventory, new AppleHeavyWeightPredicate());
+        System.out.println(heavyApples);
+
+        /**
+         * 第五次尝试: 使用匿名类
+         */
+        List<Apple> redApples2 = filterApples(inventory, new ApplePredicate() {
+            public boolean test(Apple a) {
+                return a.getColor().equals("red");
+            }
+        });
+        System.out.println(redApples2);
+
+        /**
+         * 第六次尝试：使用Lambda表达式
+         */
+        List<Apple> result = filterApples(inventory, (Apple apple) -> "red".equals(apple.getColor()));
+    }
+
+    public interface Predicate<T> {
+        boolean test(T t);
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for (T e : list) {
+            if (p.test(e)) {
+                result.add(e);
+            }
+        }
+        return list;
+    }
 }
